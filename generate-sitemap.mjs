f6a88d4a480881a93eb414366a187462730fa193
Harsh Rathore <124332768+Hxrshrathore@users.noreply.github.com>
2024-10-19 17:42:11 +0530

    Add files via upload

---
+++ generate-sitemap.mjs
@@ -0,0 +1,12 @@
+import { generateSitemap } from "next-sitemap";
+
+generateSitemap({
+  siteUrl: "https://pandav.studio",
+  outDir: "./out",
+})
+  .then(() => {
+    console.log("Sitemap generated successfully.");
+  })
+  .catch((err) => {
+    console.error("Error generating sitemap:", err);
+  });
